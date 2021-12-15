800e1f52dd693cf9888f203b54bbec963e9fa985
xucheng <jp.kimitsu@gmail.com>
2021-12-15 19:42:08 +0900

    update

---
+++ cmd/merge.go
@@ -0,0 +1,78 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"log"
+
+	"github.com/signintech/gopdf"
+	"github.com/spf13/cobra"
+)
+
+var dir string
+
+type pdf struct {
+}
+
+// mergeCmd represents the merge command
+var mergeCmd = &cobra.Command{
+	Use:   "merge",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		pdf := gopdf.GoPdf{}
+		pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
+		pdf.AddPage()
+		err := pdf.AddTTFFont("wts11", "wts11.ttf")
+		if err != nil {
+			log.Print(err.Error())
+			return
+		}
+
+		err = pdf.SetFont("wts11", "", 14)
+		if err != nil {
+			log.Print(err.Error())
+			return
+		}
+		pdf.AddOutline("1")
+		pdf.Cell(nil, "1")
+		pdf.AddPage()
+		pdf.AddOutline(`  2`)
+		pdf.Cell(nil, "2")
+		pdf.WritePdf("hello.pdf")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(mergeCmd)
+
+	mergeCmd.Flags().StringVarP(&dir, "dir", "d", "", "Enter the directory.")
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// mergeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// mergeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
